2f30d0afe1c65e3f68b68706fc0070bc8590cd0b
lostinfo <1225039937@qq.com>
2020-04-09 16:35:23 +0800

    1

---
+++ app/Request/Admin/RoleStoreRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Request\Admin;
+
+use Hyperf\Validation\Request\FormRequest;
+use Hyperf\Validation\Rule;
+
+class RoleStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        $id = $this->input('id');
+        return [
+            'name'        => [
+                'required',
+                Rule::unique('roles')->where(function ($query) use ($id) {
+                    if ($id) {
+                        $query->where('id', '<>', $id);
+                    }
+                })
+            ],
+            'permissions' => 'required|array',
+            'menus'       => 'required|array',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name'        => '名称',
+            'permissions' => '权限',
+            'menus'       => '菜单',
+        ];
+    }
+}
