38a2a4d09922c1f09e032d66cd6710ba75b6d94b
lostinfo <1225039937@qq.com>
2020-07-13 16:18:12 +0800

    1

--- resources/backend/js/components/Menu.vue
+++ resources/backend/js/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <el-menu :default-active="defaultActive" style="border-radius: 0;"
-           :router="true" :unique-opened="true" :collapse="collapse">
+           :router="true" :unique-opened="true" :collapse="collapse" class="app-menu">
     <template v-for="(item, index) in menus">
       <template v-if="item.unfolded">
         <el-submenu :index="index + ''">
@@ -53,5 +53,22 @@
   .menu-icon {
     width: 1em;
     text-align: center;
+  }
+
+  .app-menu.el-menu.el-menu--collapse {
+    width: 48px;
+  }
+
+  .app-menu.el-menu > .el-menu-item, .app-menu.el-menu > .el-submenu .el-submenu__title {
+    padding-left: 17px !important;
+  }
+
+  .app-menu.el-menu.el-menu--collapse .el-menu-item {
+    padding: 0 17px !important;
+  }
+
+  .app-menu.el-menu.el-menu--collapse .el-menu-item .el-tooltip,
+  .app-menu.el-menu.el-menu--collapse .el-submenu .el-submenu__title {
+    padding: 0 17px !important;
   }
 </style>
